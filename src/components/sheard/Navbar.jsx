743788ea0af281bcbe8d9b7715408a78b15cc9be
skSadi12 <sksadi433@gmail.com>
2025-05-25 01:43:29 +0600

    some change in navbar

--- src/components/sheard/Navbar.jsx
+++ src/components/sheard/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, } from "react";
+import React, { useState } from "react";
 
 import {
   AppBar,
@@ -22,7 +22,14 @@ function Navbar() {
   const [open, setOpen] = useState(false);
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-  const menuItems = ["Dashboard", "Incidents", "Locations", "Activities","Documents","Cypher AI"];
+  const menuItems = [
+    "Dashboard",
+    "Incidents",
+    "Locations",
+    "Activities",
+    "Documents",
+    "Cypher AI",
+  ];
 
   return (
     <div className="border-b-2 border-black">
@@ -32,8 +39,7 @@ function Navbar() {
         sx={{
           backgroundColor: "#E4E4E7",
           color: "black",
-          paddingY:"10px"
-          
+          paddingY: "10px",
         }}
       >
         <Toolbar>
@@ -42,10 +48,10 @@ function Navbar() {
               <div>
                 <img src={logo} alt="Logo" className="w-full" />
               </div>
-              
+
               <div>
                 <IconButton color="inherit" onClick={() => setOpen(true)}>
-                  <MenuIcon sx={{minHeight:"50px",minWidth:"40px"}} />
+                  <MenuIcon sx={{ minHeight: "50px", minWidth: "40px" }} />
                 </IconButton>
               </div>
             </div>
@@ -54,65 +60,78 @@ function Navbar() {
               style={{
                 display: "flex",
                 alignItems: "center",
-                justifyItems:"center",
+                justifyItems: "center",
                 width: "100%",
-                paddingRight:"40px",
-                paddingLeft:"40px",
+                paddingRight: "40px",
+                paddingLeft: "40px",
                 justifyContent: "space-between",
-                gap:"10px"
+                gap: "10px",
               }}
             >
               <Typography sx={{ flexGrow: 1 }}>
-                <img src={logo} alt="Logo" className="text-[#0FF1F6] md:w-full lg:w-36 min-h-10"   />
+                <img
+                  src={logo}
+                  alt="Logo"
+                  className="text-[#0FF1F6] md:w-full lg:w-36 min-h-10"
+                />
               </Typography>
               <div className="flex gap-2 lg:gap-20 items-center md:gap-1">
                 <div className="flex flex-wrap gap-2 ">
-                
                   <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
-                      <Button color="inherit" sx={{fontWeight:"bold"}} >
-                        <Link to="/">Dashboard</Link>
-                      </Button>
-                    </div>
-                  <div className="active:font-bold active:text-black
-                   text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
-                      <Button color="inherit"  sx={{fontWeight:"bold"}} >
-                        <Link to="/incidents" >Incidents</Link>
-                      </Button>
-                    </div>
+                    <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                      <Link to="/">Dashboard</Link>
+                    </Button>
+                  </div>
+                  <div
+                    className="active:font-bold active:text-black
+                   text-[#A1A1AA] font-semibold active:border-b-3 active:border-black"
+                  >
+                    <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                      <Link to="/incidents">Incidents</Link>
+                    </Button>
+                  </div>
                   <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
-                      <Button color="inherit"  sx={{fontWeight:"bold"}} >
-                        <Link to="">Locations</Link>
-                      </Button>
-                    </div>
+                    <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                      <Link to="">Locations</Link>
+                    </Button>
+                  </div>
                   <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
-                      <Button color="inherit"  sx={{fontWeight:"bold"}} >
-                        <Link to="">Activities</Link>
-                      </Button>
-                    </div>
+                    <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                      <Link to="">Activities</Link>
+                    </Button>
+                  </div>
                   <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
-                      <Button color="inherit"  sx={{fontWeight:"bold"}} >
-                        <Link to="">Documents</Link>
-                      </Button>
-                    </div>
+                    <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                      <Link to="">Documents</Link>
+                    </Button>
+                  </div>
                   <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
-                      <Button color="inherit"  sx={{fontWeight:"bold"}} >
-                        <Link to="">Cypher AI</Link>
-                      </Button>
-                    </div>
+                    <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                      <Link to="">Cypher AI</Link>
+                    </Button>
+                  </div>
                 </div>
                 <Typography>
                   <div className="flex gap-2">
                     <div className="flex items-center gap-2">
-                      <button className=" cursor-pointer " >
-                        <img src="/navring.png" alt="" className="min-h-[50px] min-w-[50px]"/>
+                      <button className=" cursor-pointer ">
+                        <img
+                          src="/navring.png"
+                          alt=""
+                          className="min-h-[50px] min-w-[50px]"
+                        />
                       </button>
                       <button className=" cursor-pointer ">
-                        <img src="/navpic.png" alt="" className="min-h-[50px] min-w-[50px]" />
+                        <img
+                          src="/navpic.png"
+                          alt=""
+                          className="min-h-[50px] min-w-[50px]"
+                        />
                       </button>
 
                       <div className="text-[#A1A1AA] hover:text-black ">
                         <p className="font-semibold">Usman Zafar</p>
-                        
+
                         <p className="text-[14px]">usmanzafar@gmail.com</p>
                       </div>
                     </div>
@@ -129,33 +148,63 @@ function Navbar() {
           sx={{
             backgroundColor: "#E4E4E7",
             color: "black",
-            minHeight:"100vh"
+            minHeight: "100vh",
           }}
         >
           <ListItem>
-            <img src={logo} alt="Logo"  className="min-h-10"/>
+            <img src={logo} alt="Logo" className="min-h-10" />
           </ListItem>
-        
-            <hr />
-        
-          {menuItems.map((item) => (
-            <ListItem key={item} onClick={() => setOpen(false)}>
-              <ListItemButton>
-                <ListItemText primary={item} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+
+          <hr />
+
+          <ListItem>
+            <div className="flex flex-col gap-2 ">
+              <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
+                <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                  <Link to="/">Dashboard</Link>
+                </Button>
+              </div>
+              <div
+                className="active:font-bold active:text-black
+                   text-[#A1A1AA] font-semibold active:border-b-3 active:border-black"
+              >
+                <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                  <Link to="/incidents">Incidents</Link>
+                </Button>
+              </div>
+              <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
+                <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                  <Link to="">Locations</Link>
+                </Button>
+              </div>
+              <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
+                <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                  <Link to="">Activities</Link>
+                </Button>
+              </div>
+              <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
+                <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                  <Link to="">Documents</Link>
+                </Button>
+              </div>
+              <div className="active:font-bold active:text-black text-[#A1A1AA] font-semibold active:border-b-3 active:border-black">
+                <Button color="inherit" sx={{ fontWeight: "bold" }}>
+                  <Link to="">Cypher AI</Link>
+                </Button>
+              </div>
+            </div>
+          </ListItem>
           <div className="flex-col">
             <ListItem>
               <div className="flex gap-2">
-                    <div className="flex flex-col-reverse items-center gap-2">
-                      <div className="text-[#A1A1AA] hover:text-black ">
-                        <p className="font-semibold">Usman Zafar</p>
-                        
-                        <p className="text-[14px]">usmanzafar@gmail.com</p>
-                      </div>
-                    </div>
+                <div className="flex flex-col-reverse items-center gap-2">
+                  <div className="text-[#A1A1AA] hover:text-black ">
+                    <p className="font-semibold">Usman Zafar</p>
+
+                    <p className="text-[14px]">usmanzafar@gmail.com</p>
                   </div>
+                </div>
+              </div>
             </ListItem>
           </div>
           <ListItem onClick={() => setOpen(false)}></ListItem>
@@ -165,4 +214,4 @@ function Navbar() {
   );
 }
 
-export default Navbar; 
+export default Navbar;

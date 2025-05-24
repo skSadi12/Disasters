5be846d43c06e3619f6102b266ddf6d8b3f282e4
skSadi12 <sksadi433@gmail.com>
2025-05-25 01:51:52 +0600

    footer added

---
+++ src/components/sheard/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bg-[#E4E4E7] text-center justify-center items-center p-5 mt-10 flex gap-3 md:gap-5 flex-col md:flex-row">
+      <p>Copyright © 2025 - 2029 .</p>
+      <p>Terms Disasters® .</p>
+      <p>All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,16 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/sheard/Navbar";
+import Footer from "../components/sheard/Footer";
 
 
 const Main = () => {
   return (
     <div className="font-sans antialiased  tetx-black  ">
-      
       <Navbar/>
 
       <Outlet></Outlet>
 
-      
+      <Footer/>
     </div>
   );
 };
